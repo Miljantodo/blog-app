dea8fe6acd5740bb791dd98b82623acae6955589
Miljan Todorovic <miljantodorovic@outlook.com>
2023-02-25 20:58:59 +0100

    Added Navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Posts from "./pages/Posts";
+import Users from "./pages/Users";
+import Root from "./pages/Root";
 
 const App = () => {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: <Users /> },
+        { path: "posts", element: <Posts /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
